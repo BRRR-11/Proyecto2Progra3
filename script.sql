a7fa4d6c9bd10ac435ab96927bc9e932202844c0
ArianaQ21 <quesadaariana514@gmail.com>
2021-11-01 14:21:40 -0600

    scriptProperties

---
+++ script.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE Proyecto2;
+
+use Proyecto2;
+
+create table Cliente (
+       nombre varchar(30) not null,  
+       password  varchar(30)  not null,
+       Primary Key (nombre)         
+     );
+
+
+
